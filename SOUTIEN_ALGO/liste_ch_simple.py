e018b4226f51deb8c2802f4e7c071ac410cb1073
pierreleguen <you@example.com>
2018-10-16 17:54:56 +0200

    add

--- SOUTIEN_ALGO/liste_ch_simple.py
+++ SOUTIEN_ALGO/liste_ch_simple.py
@@ -9,7 +9,7 @@ class ListeCh_Simple():
     def affListeCh(self):
         courant = self.tete
         while courant != None:
-            print(courant.val, end = " -- > ")
+            print(courant.val,end=' => ')
             courant = courant.suiv
     def ajoutTete(self, valeurTete):
         mx = Maillon(valeurTete)
@@ -23,6 +23,22 @@ class ListeCh_Simple():
         else:
             while courant.suiv != None:
                 courant = courant.suiv
+            courant.suiv=mf
+    def ajoutPos(self, position, valeur):
+        pos_actuel = 0
+        courant = self.tete
+        mp = Maillon(valeur)
+        if courant == None:
+            self.tete=mp
+        else:
+            while courant.suiv and pos_actuel < position:
+                pos_actuel+=1
+                courant_prec=courant
+                courant = courant.suiv
+            courant_prec.suiv=mp
+            mp.suiv=courant
+
+        
 
 
 m = Maillon("5")
@@ -31,4 +47,7 @@ l = ListeCh_Simple(m)
 l.ajoutTete("1")
 #l.ajoutTete("2")
 l.ajoutFin("10")
+l.ajoutFin("20")
+l.ajoutFin("30")
+l.ajoutPos(2,"ajout")
 l.affListeCh()
